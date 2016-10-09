ed2aed192cd53463b9023f3464002c783b619fa7
awiouy <awiouy@gmail.com>
2016-10-09 12:40:31 +0200

    invert nc client/server

--- docker/run.sh
+++ docker/run.sh
@@ -9,10 +9,6 @@ xset -dpms
 xset s off
 xset s noblank
 
-nc -l -u -p 1234|./tmp/input.sh &
+nc -u 127.0.0.1 1234 | ./tmp/input.sh &
 
-chromium-browser --verbose --no-sandbox --user-data-dir="/storage/.config/chromium-browser" --start-maximized http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4 &
-
-while ps axg | grep -vw grep | grep -w chromium-browser > /dev/null; do sleep 1; done
-
-exit
+chromium-browser --verbose --no-sandbox --user-data-dir="/storage/.config/chromium-browser" --start-maximized http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4
