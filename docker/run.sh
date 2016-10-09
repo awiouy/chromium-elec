1effd6ffa47dc6ed35b48214e2d4a4603afa865f
awiouy <awiouy@gmail.com>
2016-10-09 19:38:18 +0200

    Revert "invert nc client/server"
    This reverts commit ed2aed192cd53463b9023f3464002c783b619fa7.

--- docker/run.sh
+++ docker/run.sh
@@ -9,6 +9,10 @@ xset -dpms
 xset s off
 xset s noblank
 
-nc -u 127.0.0.1 1234 | ./tmp/input.sh &
+nc -l -u -p 1234|./tmp/input.sh &
 
-chromium-browser --verbose --no-sandbox --user-data-dir="/storage/.config/chromium-browser" --start-maximized http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4
+chromium-browser --verbose --no-sandbox --user-data-dir="/storage/.config/chromium-browser" --start-maximized http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4 &
+
+while ps axg | grep -vw grep | grep -w chromium-browser > /dev/null; do sleep 1; done
+
+exit
